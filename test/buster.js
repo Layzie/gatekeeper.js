41e0850146237bfb908255452e3d2d61a0ee45fa
HIRAKI Satoru <saruko313@gmail.com>
2012-12-12 17:13:45 +0900

    fix test file path

--- test/buster.js
+++ test/buster.js
@@ -3,7 +3,7 @@ var config = module.exports;
 config['GatekeeperTest'] = {
   env: 'browser',
   rootPath: '../',
-  sources: ['src/*.coffee'],
-  tests: ['test/*-test.coffee'],
+  sources: ['src/**/*.coffee'],
+  tests: ['test/**/*-test.coffee'],
   extensions: [require("buster-coffee")]
 };
