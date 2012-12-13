7feeda461958ec9fa9c58ec79e1b850f8ab442e1
HIRAKI Satoru <saruko313@gmail.com>
2012-12-13 12:54:00 +0900

    fix clean path

--- grunt.js
+++ grunt.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
       }
     },
     clean: {
-      build: ['src/**/*.js', 'test/**/*-test.js']
+      build: ['src/*.js', 'test/*-test.js']
     },
     coffee: {
       compile: {
