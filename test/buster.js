cd2467b74d142ad74a01e8b57df358a2c933cd90
HIRAKI Satoru <saruko313@gmail.com>
2012-12-27 21:22:35 +0900

    remove test directory
    Because of there are no test yet.

--- test/buster.js
+++
@@ -1,9 +0,0 @@
-var config = module.exports;
-
-config['GatekeeperTest'] = {
-  env: 'browser',
-  rootPath: '../',
-  sources: ['src/**/*.coffee'],
-  tests: ['test/**/*-test.coffee'],
-  extensions: [require("buster-coffee")]
-};
